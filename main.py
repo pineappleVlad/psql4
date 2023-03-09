84c45ca13041f5d1eef501af4e34e9650dbed0ff
Vladislav Shumilo <vlad.shumilo.2014@mail.ru>
2023-03-09 21:19:03 +0700

    change work

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import psycopg2
-
-
 def create_db(cur):
     cur.execute("""
     CREATE TABLE IF NOT EXISTS clients(
@@ -18,7 +16,6 @@ def create_db(cur):
     phone BIGINT
     );
     """)
-    conn.commit()
 
 
 def drop_db(cur):
@@ -26,8 +23,6 @@ def drop_db(cur):
     DROP TABLE phones;
     DROP TABLE clients;
     """)
-
-    conn.commit()
 
 def new_client(cur, first_name, last_name, email):
     cur.execute("""
@@ -45,18 +40,23 @@ def add_phone(cur, client_id, phone):
     print(cur.fetchall())
 
 
-def change_data(cur, client_id=None, first_name=None, last_name=None, email=None):
-    cur.execute("""
-    UPDATE clients SET first_name = '%s', last_name = '%s', email = '%s' WHERE id = %s 
-    RETURNING *;
-    """%(first_name, last_name, email, client_id))
+def change_data(cur, client_id, first_name=None, last_name=None, email=None):
+    if first_name != None:
+        cur.execute("""
+        UPDATE clients SET first_name = '%s' WHERE id = %s 
+        RETURNING *;
+        """%(first_name, client_id))
+    if last_name != None:
+        cur.execute("""
+        UPDATE clients SET last_name = '%s' WHERE id = %s 
+        RETURNING *;
+        """%(last_name, client_id))
+    if email != None:
+        cur.execute("""
+        UPDATE clients SET email = '%s' WHERE id = %s 
+        RETURNING *;
+        """%(email, client_id))
     print(cur.fetchall())
-
-    # cur.execute("""
-    # UPDATE phones SET phone = %s WHERE client_id = %s
-    # RETURNING *;
-    # """%(phones, client_id))
-    # print(cur.fetchall())
 
 
 def del_phone(cur, client_id, phone):
@@ -64,8 +64,6 @@ def del_phone(cur, client_id, phone):
     DELETE FROM phones
 	WHERE id = %s AND phone = %s;
     """%(client_id, phone))
-    # print(cur.fetchall())
-    conn.commit
 
     cur.execute("""
     SELECT * FROM phones
@@ -78,29 +76,28 @@ def del_client(cur, client_id):
     DELETE FROM phones
     WHERE client_id = %s
     """%(client_id))
-    conn.commit()
-
-    # cur.execute("""
-    # SELECT * FROM phones
-    # """)
-    # print(cur.fetchall())
 
     cur.execute("""
     DELETE FROM clients
 	WHERE id = %s;
     """%(client_id))
-    conn.commit()
-
-    # cur.execute("""
-    # SELECT * FROM clients
-    # """)
-    # print(cur.fetchall())
 
 def find_client(cur, first_name=None, last_name=None, email=None):
-    cur.execute("""
-    SELECT * FROM clients
-	WHERE first_name = '%s' OR last_name = '%s' OR email = '%s';
-    """%(first_name, last_name, email))
+    if first_name != None:
+        cur.execute("""
+        SELECT * FROM clients
+       	WHERE first_name = '%s';
+        """%(first_name))
+    if last_name != None:
+        cur.execute("""
+        SELECT * FROM clients
+        WHERE last_name = '%s';
+        """%(last_name))
+    if email != None:
+        cur.execute("""
+        SELECT * FROM clients
+    	WHERE email = '%s';
+        """%(email))
     print(cur.fetchall())
 
 def find_client_using_phone(cur, phone):
@@ -111,18 +108,20 @@ def find_client_using_phone(cur, phone):
     """%(phone))
     print(cur.fetchall())
 
-with psycopg2.connect(database='clients_db', user='postgres', password='doka7744') as conn:
-    with conn.cursor() as cur:
-        drop_db(cur)
-        create_db(cur)
-        new_client(cur, 'vasya', 'pupkin', 'pupkin@mail.ru')
-        add_phone(cur, 1, 89232323233)
-        add_phone(cur, 1, 89333333333)
-        change_data(cur, 1, 'Vasyliy', 'pupkevich', 'pupkins@bk.ru')
-        change_data(cur, 1, 'Vitaliy')
-        del_phone(cur, 1, 89232323233)
-        del_client(cur, 1)
-        find_client(cur, 'vasya')
-        find_client_using_phone(cur, 89333333333)
 
-conn.close()
+if __name__ == "__main__":
+    with psycopg2.connect(database='clients_db', user='postgres', password='doka7744') as conn:
+        with conn.cursor() as cur:
+            drop_db(cur)
+            create_db(cur)
+            new_client(cur, 'vasya', 'pupkin', 'pupkin@mail.ru')
+            new_client(cur, 'vasya', 'golenkov', 'gol@mail.ru')
+            add_phone(cur, 1, 89232323233)
+            add_phone(cur, 2, 89333333333)
+            change_data(cur, 1, 'Vasyliy', 'pupkevich', 'pupkins@bk.ru')
+            change_data(cur, 1, 'Vitaliy')
+            del_phone(cur, 1, 89232323233)
+            del_client(cur, 1)
+            find_client(cur, 'vasya')
+            find_client_using_phone(cur, 89333333333)
+    conn.close()
